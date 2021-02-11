132224739063a2cc34cb00b5801f2dddbbe998fe
Dan Pappo <dpappo@gmail.com>
2021-02-11 15:51:22 -0500

    refactored with extending classes, for DRY code

--- index.js
+++ index.js
@@ -1,29 +1,59 @@
 let balance = 500.00;
 
-class Withdrawal {
-
-  constructor(amount) {
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
+  }
+  commit() {
+    this.account.balance += this.value;
   }
 
-  commit() {
-    balance -= this.amount;
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+
+}
+
+class Deposit extends Transaction {
+
+
+  get value() {
+    return this.amount;
+  }
+
+}
+
+class Account {
+
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
   }
 
 }
 
 
 
-
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("snow-patrol");
+
+const t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+const t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+
+// console.log('Balance:', balance);
